50f00c02de69bee3d60224aa2dd9c844a33d45e7
jlee@viewgol.com <jlee@viewgol.com>
2021-05-26 14:49:49 +0000

    interim checkin

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,14 +1,9 @@
 with customers as (
-
     select * from {{ref('stg_customers')}}
-
 ),
 
 orders as (
-
-        select * from {{ref('stg_orders')}}
-
-
+        select * from {{ref('fct_orders')}}
 ),
 
 customer_orders as (
@@ -18,10 +13,9 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
 
 ),
